9561dceba3485554b417aac1b9e3dc0b0d1df7f6
Matvey <matvey.kochegarov@mail.ru>
2026-04-29 23:06:14 +0300

    first comm

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,70 @@
+// internal/handlers/handlers.go
 package handlers
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+// GetHTML возвращает HTML форму
+func GetHTML(w http.ResponseWriter, r *http.Request) {
+	content, err := os.ReadFile("../index.html")
+	if err != nil {
+		http.Error(w, "Unable to read file", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write(content)
+}
+
+func UploadHTML(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Unable to parse form", http.StatusInternalServerError)
+		return
+	}
+
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		http.Error(w, "Unable to get file from form", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	fileContent, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, "Unable to read file content", http.StatusInternalServerError)
+		return
+	}
+
+	convertedContent := service.TextConverter(string(fileContent))
+	if convertedContent == "" {
+		http.Error(w, "Unable empty string", http.StatusInternalServerError)
+		return
+	}
+
+	timestamp := time.Now().UTC().Format("2006-01-02_15-04-05")
+	ext := filepath.Ext(handler.Filename)
+	outputFilename := fmt.Sprintf("%s%s", timestamp, ext)
+
+	err = os.WriteFile(outputFilename, []byte(convertedContent), 0755)
+	if err != nil {
+		http.Error(w, "Unable to write in output file", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf(
+		"Original file: %s\nSaved as: %s\n\nResult:\n\n%s",
+		handler.Filename, outputFilename, convertedContent,
+	)))
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,27 @@
 package service
+
+import (
+	"log"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func TextConverter(text string) string {
+	trimmedWord := strings.TrimSpace(text)
+	if len(trimmedWord) == 0 {
+		log.Println("Ошибка, передана пустая строка")
+		return ""
+	}
+	isMorse := true
+	for _, ch := range trimmedWord {
+		if ch != '.' && ch != '-' && ch != ' ' {
+			isMorse = false
+			break
+		}
+	}
+	if isMorse {
+		return morse.ToText(trimmedWord)
+	}
+	return morse.ToMorse(trimmedWord)
+}
